d0cd090b61c611bde0b9b092a1b77ccf1527740e
Ahmed Gaber <51884558+x300n@users.noreply.github.com>
2020-08-30 22:55:41 +0200

    Update main.tf

--- aws-twotier/main.tf
+++ aws-twotier/main.tf
@@ -129,7 +129,7 @@ resource "aws_instance" "web" {
 # we specified
   ami = var.aws_amis[var.aws_region]
 
-  # The name of our SSH keypair we created above.
+  # The name of our SSH keypair we created above
   key_name = aws_key_pair.auth.id
 
   # Our Security group to allow HTTP and SSH access
@@ -140,8 +140,8 @@ resource "aws_instance" "web" {
   # backend instances.
   subnet_id = aws_subnet.default.id
 
-  # We run a remote provisioner on the instance after creating it.
-  # In this case, we just install nginx and start it. By default,
+  # We run a remote provisioner on the instance after creating it
+  # In this case, we just install nginx and start it. By default
   # this should be on port 80
   provisioner "remote-exec" {
     inline = [
